9d3efe9272c3fa51ca1ba7e963a8c6f07b99074e
AVoss <a.vosseler@gmx.net>
2025-01-04 21:40:26 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='my_package',
     version='0.0.1',
-    packages= find_packages(),
-    install_requires=[]  
+    packages=find_packages(where="src"),
+    install_requires=[],
+    package_dir={"": "src"},
 )
